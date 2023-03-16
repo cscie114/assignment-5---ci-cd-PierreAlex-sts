759c290c114a3ba47393e5d343bf83c7fcc97ecb
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-03-16 11:21:06 -0400

    restapi pagination done

--- src/_data/parks.js
+++ src/_data/parks.js
@@ -1,6 +1,8 @@
 // Javascript from assigment 2 to fetch all the parks from the NPS API
 require('dotenv').config();
 const eleventyFetch = require("@11ty/eleventy-fetch");
+const cacheDuration = '1d';
+const limit = 100;
 
 module.exports = async function() {
    
@@ -8,37 +10,66 @@ module.exports = async function() {
     // use the userAgent to let know the request comes from the browser
     let userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:102.0) Gecko/20100101 Firefox/102.0';
 
-    // params for specifi queries value 
-    let limit = 100;
-
     // Definie park params to fetch specific query
     let parksParams = {
-        limit: limit
+        limit: limit,
+        start: 0
     };
     
 
     // fetch the API by constructing the url via params
-    try {
+    // try {
+    //     let params = new URLSearchParams(parksParams);
+    //     let queryString = params.toString();
+    //     let url = baseUrl + "?" + queryString;
+    //     let parkData = await eleventyFetch(url, {
+    //         directory: ".cache",
+    //         fetchOptions: {
+    //             headers: {
+    //                 "User-agent": userAgent,
+    //                 "x-api-key": process.env.NPS_API_KEY
+    //             },
+    //         },
+    //         duration: "1d",
+    //         type: "json"
+    //     }).then((jsonData) => {
+    //         return jsonData;
+    //     });
+    //     return(parkData);
+
+    // } catch(err) {
+    //     console.error("something wrong with the api");
+    //     console.log(err);
+    // }
+    
+
+    let parks = {data:[]}
+    let nextLink = '';
+    let total = '';
+    do {
         let params = new URLSearchParams(parksParams);
         let queryString = params.toString();
         let url = baseUrl + "?" + queryString;
-        let parkData = await eleventyFetch(url, {
-            directory: ".cache",
-            fetchOptions: {
-                headers: {
-                    "User-agent": userAgent,
-                    "x-api-key": process.env.NPS_API_KEY
+        try {
+            let responseData = await eleventyFetch(url, {
+                duration: cacheDuration,
+                fetchOptions: {
+                    headers: {
+                        "User-agent": userAgent,
+                        "x-api-key": process.env.NPS_API_KEY
+                    },
                 },
-            },
-            duration: "1d",
-            type: "json"
-        }).then((jsonData) => {
-            return jsonData;
-        });
-        return(parkData);
+                type: "json"
+            });
+            total = responseData.total;
+            parks.data.push(...responseData.data);
+            nextLink = responseData.start;
+            parksParams.start += limit;
+            } catch (err) {
+                console.log("something wrong with api\n");
+                console.log(err);
+        }
+    } while ( nextLink <= total)
 
-    } catch(err) {
-        console.error("something wrong with the api");
-        console.log(err);
-    }
+    return(parks);
 };
