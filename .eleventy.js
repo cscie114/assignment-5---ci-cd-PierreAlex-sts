8e96d058d997faa7035506fa5f20c83aa105aefc
PierreAlex-sts <pierre.alexandre.journes@gmail.com>
2023-04-25 20:58:20 -0400

    failed at serverless with 11ty

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,11 @@
+const { EleventyServerlessBundlerPlugin } = require("@11ty/eleventy");
+
 module.exports = function(eleventyConfig) {
+	eleventyConfig.addPlugin(EleventyServerlessBundlerPlugin, {
+		name: "alerts",
+		functionsDir: "./netlify/functions/",
+	});
 	eleventyConfig.addPassthroughCopy("src/assets/**");
-	
 	return {
 		dir: {
 			input: "src",
